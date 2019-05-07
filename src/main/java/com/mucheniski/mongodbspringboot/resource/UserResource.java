0f26bcbadfefe8e30484e86551fad349a8a06dfa
Diego Mucheniski <mucheniski@gmail.com>
2019-05-07 15:16:15 -0300

    276 - Insert de User com POST

--- src/main/java/com/mucheniski/mongodbspringboot/resource/UserResource.java
+++ src/main/java/com/mucheniski/mongodbspringboot/resource/UserResource.java
@@ -1,5 +1,6 @@
 package com.mucheniski.mongodbspringboot.resource;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,8 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.mucheniski.mongodbspringboot.dto.UserDTO;
 import com.mucheniski.mongodbspringboot.model.User;
@@ -32,6 +36,14 @@ public class UserResource {
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {	
 		User user = userService.findById(id);		
 		return ResponseEntity.ok().body(new UserDTO(user));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody UserDTO userDTO) {	
+		User user = userService.fromDTO(userDTO);
+		user = userService.insert(user);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	
 }
--- src/main/java/com/mucheniski/mongodbspringboot/service/UserService.java
+++ src/main/java/com/mucheniski/mongodbspringboot/service/UserService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.mucheniski.mongodbspringboot.dto.UserDTO;
 import com.mucheniski.mongodbspringboot.exception.ObjectNotFoundException;
 import com.mucheniski.mongodbspringboot.model.User;
 import com.mucheniski.mongodbspringboot.repository.UserRepository;
@@ -23,6 +24,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> user = userRepository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado!"));
+	}
+	
+	public User insert(User user) {
+		return userRepository.insert(user);
+	}
+	
+	public User fromDTO(UserDTO userDTO) {
+		return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
 	}
 	
 }
