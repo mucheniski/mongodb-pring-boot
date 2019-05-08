b2a546bac4eced9b93fbf70598754724b9dea914
Diego Mucheniski <mucheniski@gmail.com>
2019-05-08 09:03:43 -0300

    281 - Projecao dos dados do Author com DTO

--- src/main/java/com/mucheniski/mongodbspringboot/configuration/Instantiation.java
+++ src/main/java/com/mucheniski/mongodbspringboot/configuration/Instantiation.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.mucheniski.mongodbspringboot.dto.AuthorDTO;
 import com.mucheniski.mongodbspringboot.model.Post;
 import com.mucheniski.mongodbspringboot.model.User;
 import com.mucheniski.mongodbspringboot.repository.PostRepository;
@@ -37,8 +38,8 @@ public class Instantiation implements CommandLineRunner {
 		User livia = new User(null, "Livia", "livia@mucheniski.com");		
 		userRepository.saveAll(Arrays.asList(bruna, diego, miguel, livia));
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2019"), "Partiu Viagem", "Vou viajar para Cuiaba, abraços", bruna);
-		Post post2 = new Post(null, sdf.parse("22/03/2019"), "Feliz", "Bom dia, acordei feliz hoje!", bruna);
+		Post post1 = new Post(null, sdf.parse("21/03/2019"), "Partiu Viagem", "Vou viajar para Cuiaba, abraços", new AuthorDTO(bruna));
+		Post post2 = new Post(null, sdf.parse("22/03/2019"), "Feliz", "Bom dia, acordei feliz hoje!", new AuthorDTO(bruna));
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 	}
